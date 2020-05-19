7ac5546a02dd12447c32bc531f68c72ea29c8376
shivamvk <shivam.vk529@gmail.com>
2020-05-20 00:21:52 +0530

    learn useReducer hook in input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import Input from '../../shared/components/FormElements/Input';
+import Input from "../../shared/components/FormElements/Input";
 import "./NewPlace.css";
 
-const NewPlace = props => {
-    return <form className="place-form">
-        <Input element="input" type="text" label="Title"/>
+const NewPlace = (props) => {
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        errorText="This shouldn't be empty"
+      />
     </form>
-}
+  );
+};
 
 export default NewPlace;
